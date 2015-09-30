c07b0212a6d6ba0bebd7640a9e98a66b6393d309
Sabadam_bobdi <prevostdjonko@yahoo.fr>
2015-09-30 12:24:45 -0400

    aop formation

---
+++ sprintAOP/src/main/java/App.java
@@ -0,0 +1,33 @@
+
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import service.FooService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main(String[] args) {
+		 
+        // Create Spring application context that configured by xml.
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:/spring.xml");
+        // Get our service from the spring context (that we want to be profiled).
+        FooService fooService = ctx.getBean(FooService.class);
+ 
+        // Test profiling through methods calls.
+ 
+        for (int i = 0; i < 10; i++) {
+            fooService.doShortJob();
+        }
+        fooService.doLongJob();
+ 
+        // Test that profiler also can handle Exceptions in profiled method.
+        fooService.doSomethingThatThrowsException();
+ 
+        // Close the spring context
+        ctx.close();
+    }
+}
---
+++ sprintAOP/src/main/java/service/FooService.java
@@ -0,0 +1,28 @@
+package service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FooService {
+
+	
+	public void doShortJob() {
+        sleep(10l);
+    }
+ 
+    public void doLongJob() {
+        sleep(300l);
+    }
+ 
+    public void doSomethingThatThrowsException() {
+        throw new RuntimeException("Simulated Exception.");
+    }
+ 
+    private void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
